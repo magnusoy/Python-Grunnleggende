09086d2c5a590631b40fdec50a87ca0491aa9d29
Magnusoy <magnus.oye@gmail.com>
2020-09-22 18:07:39 +0200

    Fikset feil

--- Kapittel-14/binærsøk.py
+++ Kapittel-14/binærsøk.py
@@ -18,24 +18,7 @@ def binary_search(lst, number):
     """
     docstring
     """
-
-    mid = 0
-    start = 0
-    end = len(lst)
-    step = 0
-
-    while (start <= end):
-        step +=  1
-        mid = (start + end) // 2
-
-        if number == lst[mid]:
-            return mid
-
-        if number < lst[mid]:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return -1
+    pass
 
 
 numbers = random.sample(range(1, 50), 46)
